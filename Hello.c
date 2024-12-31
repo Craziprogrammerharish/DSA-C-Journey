dfb379b3c8e754611acf30604beaaae2bab24cfe
Craziprogrammerharish <98517216+Craziprogrammerharish@users.noreply.github.com>
2024-12-31 10:14:34 +0530

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>  // Preprocessor directive to include the Standard Input Output header file
+
+// The main function serves as the entry point for program execution.
+int main() {
+    // printf is a function that prints text to the console.
+    printf("Hello, World!\n");  // \n adds a newline after the message.
+
+    return 0;  // Indicates successful program execution to the operating system.
+}
